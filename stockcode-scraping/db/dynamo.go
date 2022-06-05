86503e131f1c711934e42ec4b4bcddec63cea48f
Umaxyon <umaxyon@gmail.com>
2022-06-05 21:16:45 +0900

    dynamodb.BatchWrite実装(wip)

--- stockcode-scraping/db/dynamo.go
+++ stockcode-scraping/db/dynamo.go
@@ -1,11 +1,14 @@
 package db
 
 import (
+	"context"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+	"stockcode-scraping/yh"
 )
 
 var (
@@ -31,28 +34,65 @@ func NewAccessor() *DynamoAccessor {
 	return &DynamoAccessor{db}
 }
 
-func (da *DynamoAccessor) Upsert() {
-	_, err := da.db.PutItem(&dynamodb.PutItemInput{
-		TableName: aws.String("stcode"),
-		Item: map[string]*dynamodb.AttributeValue{
-			"code": {
-				S: aws.String("1111"),
+func (da *DynamoAccessor) BatchWrite(ctx context.Context, data []yh.StockPage) error {
+	items := make([]*dynamodb.WriteRequest, 0, len(data))
+	for _, v := range data {
+		av, _ := dynamodbattribute.MarshalMap(v)
+		items = append(items, &dynamodb.WriteRequest{
+			PutRequest: &dynamodb.PutRequest{
+				Item: av,
 			},
-			"name": {
-				S: aws.String("あああ"),
-			},
-			"tel": {
-				S: aws.String("123-4567-8901"),
-			},
-		},
-	})
-	if err != nil {
-		panic(err)
+		})
 	}
+	for len(items) > 0 {
+		out, err := da.db.BatchWriteItemWithContext(ctx, &dynamodb.BatchWriteItemInput{
+			RequestItems: map[string][]*dynamodb.WriteRequest{
+				"stcode": items,
+			},
+		})
+		if err != nil {
+			return fmt.Errorf("batch write to %s: %w", "stockpage", err)
+		}
+
+		items = append(items[:0], out.UnprocessedItems["stcode"]...)
+	}
+	return nil
+}
+
+func (da *DynamoAccessor) SaveStCode(stPageList []yh.StockPage) {
+	ctx := context.Background()
+
+	for i := 0; i < len(stPageList); i += 25 {
+		end := i + 25
+		if end > len(stPageList) {
+			end = len(stPageList)
+		}
+		if err := da.BatchWrite(ctx, stPageList[i:end]); err != nil {
+			fmt.Printf("SaveStCode err i=%d, end=%d", i, end)
+		}
+	}
+
+	//_, err := da.db.PutItem(&dynamodb.PutItemInput{
+	//	TableName: aws.String("stcode"),
+	//	Item: map[string]*dynamodb.AttributeValue{
+	//		"code": {
+	//			S: aws.String("1111"),
+	//		},
+	//		"name": {
+	//			S: aws.String("あああ"),
+	//		},
+	//		"tel": {
+	//			S: aws.String("123-4567-8901"),
+	//		},
+	//	},
+	//})
+	//if err != nil {
+	//	panic(err)
+	//}
 }
 
 func (da *DynamoAccessor) Query() {
-	keyCond := expression.Key("code").Equal(expression.Value("1111"))
+	keyCond := expression.Key("Code").Equal(expression.Value("1111"))
 
 	expr, err := expression.NewBuilder().WithKeyCondition(keyCond).Build()
 	if err != nil {
--- stockcode-scraping/main.go
+++ stockcode-scraping/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"stockcode-scraping/db"
 	"stockcode-scraping/yh"
+	"strings"
 	"time"
 )
 
@@ -17,10 +20,21 @@ func handler() error {
 		return err
 	}
 
-	err = yh.ScrapingStockPageStart(ind.LinkList)
-	if err != nil {
+	stockPageList, errList := yh.ScrapingStockPageStart(ind.LinkList)
+	if errList != nil {
+		if len(errList) != 0 {
+			var errStr string
+			for i := range errList {
+				errStr += errList[i].Error() + ","
+			}
+			err = errors.New(strings.TrimRight(errStr, ","))
+		}
 		return err
 	}
+
+	accessor := db.NewAccessor()
+	accessor.SaveStCode(stockPageList)
+
 	fmt.Printf("%vms\n", time.Since(now).Milliseconds())
 	return nil
 }
